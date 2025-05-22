7bbff6d76b8194c6a6bd9714c9fc87ec41261545
Hassam-Hidayat <22bese22@gmail.com>
2025-05-22 14:32:08 +0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,8 +1,8 @@
 def create_profile(name, student_id, bio):
     with open('student_profile.txt', 'w') as f:
-        f.write(f"Name:Hassam Hidayat {name}\n")
-        f.write(f"Student ID:22SE22 {student_id}\n")
-        f.write(f"Bio:Student {bio}\n")
+        f.write(f"Name: {name}\n")
+        f.write(f"Student ID: {student_id}\n")
+        f.write(f"Bio: {bio}\n")
     print("Profile created successfully")
 
 if __name__ == "__main__":
